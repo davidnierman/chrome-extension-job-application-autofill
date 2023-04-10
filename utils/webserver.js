a82a872d4e6dcab99cc7af2602eaf3141540dca5
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-04-10 15:47:23 -0400

    Add scripts to reference dev & prod envs

--- utils/webserver.js
+++ utils/webserver.js
@@ -1,7 +1,10 @@
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
-process.env.ASSET_PATH = '/';
+// process.env.BABEL_ENV = 'development';
+// process.env.NODE_ENV = 'development';
+// process.env.ASSET_PATH = '/';
+require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` })
+
+
 
 var WebpackDevServer = require('webpack-dev-server'),
   webpack = require('webpack'),
